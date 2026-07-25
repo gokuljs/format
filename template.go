9d174644f30328938768b056415c77a5187172fd
Jo Turk <git@joturk.net>
2026-07-26 01:55:39 +0300

    Init

---
+++ template.go
@@ -0,0 +1,5 @@
+// SPDX-FileCopyrightText: 2026 The Pion community <https://pion.ly>
+// SPDX-License-Identifier: MIT
+
+// Package format implements readers and writer for media container formats.
+package format
